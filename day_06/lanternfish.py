d13a7bd1859f4ebb5f76c21900d9191df2954e3c
André van Delft <andrevandelft@outlook.com>
2021-12-13 11:07:56 +0100

    Optimize lanternfish and complete part 2

--- day_06/lanternfish.py
+++ day_06/lanternfish.py
@@ -1,37 +1,43 @@
 import re
 from pathlib import Path
-
+from collections import Counter
 
 DAY_DIR = Path(__file__).parent
 
 with open(DAY_DIR / "input.txt") as f:
-    INITIAL_STATE = f.read()
-
-TEST_INPUT = "3,4,3,1,2"
+    INPUT = f.read()
 
 
-def perform_day(counters):
-    new_counters = []
-    for counter in counters:
-        if counter == 0:
-            new_counters += [6, 8]
-        else:
-            new_counters.append(counter - 1)
-
-    return new_counters
+def parse_input(input_string):
+    return Counter(int(n) for n in re.findall("\d+", input_string))
 
 
-def part_one(initial_state=INITIAL_STATE, num_days=80):
-    counters = [int(n) for n in re.findall("\d+", initial_state)]
+def perform_day(lanternfish: Counter):
+    new_lanternfish = Counter()
+    for age, num_lanternfish in lanternfish.items():
+        if age > 0:
+            new_lanternfish[age - 1] = num_lanternfish
 
-    for _ in range(num_days):
-        counters = perform_day(counters)
+    new_lanternfish[6] = new_lanternfish.get(6, 0) + lanternfish.get(0, 0)
+    new_lanternfish[8] = lanternfish.get(0, 0)
 
-    print(f"There are {len(counters)} lanternfish after {num_days} days")
+    return new_lanternfish
 
 
-def part_two(initial_state=INITIAL_STATE, num_days=256):
-    part_one(num_days=num_days)
+def part_one(input_string=INPUT, num_days=80):
+    lanternfish = parse_input(input_string)
+    for n in range(num_days):
+        print(f"Calculating day {n + 1}")
+        print(lanternfish)
+        lanternfish = perform_day(lanternfish)
+
+    num_counters = sum(lanternfish.values())
+    print(f"There are {num_counters} lanternfish after {num_days} days")
+    return num_counters
+
+
+def part_two(input_string=INPUT, num_days=256):
+    return part_one(input_string, num_days)  # This takes a LONG time
 
 
 if __name__ == "__main__":
